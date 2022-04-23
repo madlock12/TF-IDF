f6fdfb9174eefa050aeac97410ccc34187303f31
Mir Mubashir Ali <mubashirali12350@gmail.com>
2022-04-23 20:51:00 +0500

    Update k191310.py
    TF Values are now being calculated correctly

--- k191310.py
+++ k191310.py
@@ -15,7 +15,7 @@ import string
 # create tokens and remove stop words and lemmatize
 def tokenize_remove_stopwords(data, stopwords):
     for character in string.punctuation:
-        data.replace(character," ")
+        data.replace(character, " ")
 
     retreaved = text_to_word_sequence(data)  # tokenize text file
     for i in range(len(stopwords)):
@@ -27,10 +27,10 @@ def tokenize_remove_stopwords(data, stopwords):
     return retreaved
 
 
-def populate_index(vector,stopwords):
+def populate_index(vector, stopwords):
     index = dict()
     for i in vector:
-        index.setdefault(i,[0])
+        index.setdefault(i, [0])
         for j in range(448):
             index[i].append(0)
 
@@ -40,12 +40,14 @@ def populate_index(vector,stopwords):
         if(f):
             data = (f.read())
             f.close()
-            data=tokenize_remove_stopwords(data,stopwords)
-            
-            for i in vector:
-                if(i in data):
-                    index[i][docno]+=1
+            data = tokenize_remove_stopwords(data, stopwords)
+
+            for i in data:
+                index[i][docno-1] += 1
+        else:
+            print("File failed to open")
     print(index)
+
 
 def newfile(stopwords):  # this function will create index and store it in the index.txt file
     vector = []
@@ -62,7 +64,7 @@ def newfile(stopwords):  # this function will create index and store it in the i
                     vector.append(i)
     print(vector)
     print(len(vector))
-    populate_index(vector,stopwords)
+    populate_index(vector, stopwords)
     # f = open("index.txt", "w")#here we will store all indexes
 
     # f.close()
