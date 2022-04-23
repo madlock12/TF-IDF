c52923557205aa1e5eb70ef88ba648e862e4a8bf
Mir Mubashir Ali <mubashirali12350@gmail.com>
2022-04-23 14:33:53 +0500

    Update k191310.py
    stop word is being removed and tokenization is performed

--- k191310.py
+++ k191310.py
@@ -1,41 +1,54 @@
 ############################################################################################################
 #                                    IR Assignment 1
-#                                         TF*IDF 
+#                                         TF*IDF
 #
 #                                       19k1310
 #                                   Mir Mubashir Ali
 #                                       4/23/2022
 ############################################################################################################
 import os
-import spacy
+from pydoc import doc
+from keras.preprocessing.text import text_to_word_sequence
 
-def tokenize():#create tokens
+
+def tokenize_remove_stopwords(data):  # create tokens and remove stop words
+    f = open("Stopword-List.txt", 'r')
+    if(f):
+        temp = (f.read())
+        stopwords = text_to_word_sequence(temp)
+    else:
+        print("Stop Word file does not exist")
+
+    retreaved = text_to_word_sequence(data)  # tokenize text file
+
+    for i in range(len(stopwords)):
+        if(stopwords[i] in retreaved):  # if specific stopwords is in token
+            retreaved.remove(stopwords[i])  # removing stop words
+    return retreaved
 
 
-def newfile():#this function will create index and store it in the index.txt file
-    for docno in range(1,449):
-        ff=open("./Abstracts/Abstracts/"+str(docno) +
-             ".txt", "r")  # reading all 448 files
+def newfile():  # this function will create index and store it in the index.txt file
+    for docno in range(1, 449):
+        ff = open("./Abstracts/Abstracts/"+str(docno) +
+                  ".txt", "r")  # reading all 448 files
         if(ff):
-            data=(ff.read())
+            data = (ff.read())
+            retreaved = tokenize_remove_stopwords(data)
 
-    
-    
-    f=open("index.txt","w")
-
-
+    f = open("index.txt", "w")
 
     f.close()
 
-file=open("index.txt","r")
-if(file):#if file exist
-    filesize=os.path.getsize("index.txt")
-    if(filesize==0):#file is empty hence we have to create index and store
+
+file = open("index.txt", "r")
+if(file):  # if file exist
+    filesize = os.path.getsize("index.txt")
+    if(filesize == 0):  # file is empty hence we have to create index and store
         os.remove("index.txt")
         newfile()
-else: #if file does not exist
+else:  # if file does not exist
     newfile()
 
-f=open("index.txt","r")
+f = open("index.txt", "r")
 if(f):
-    index=(f.read())
+    index = (f.read())
